91960412243727390f8b1f7f95699c7b2c5f73c5
yoko6510 <yoko6510@GitHubpomme0damour@gmail.com>
2026-04-10 17:59:51 +0900

    ホバー効果をボタンに移動＋BRUNO商品の説明文を削除
    - カード全体のホバー浮き上がり効果を削除
    - 楽天ボタン・Amazonボタンに個別のホバー浮き上がり効果を追加
    - 各ボタンのブランドカラーに合わせた影の色に調整
    - BRUNO商品の説明文を非表示に

--- data.js
+++ data.js
@@ -67,7 +67,7 @@ var SITE_DATA = {
     {
       number:      1,
       title:       "BRUNO コンパクトホットプレート",
-      description: "一人用〜パーティまで。プレート2種・最大250℃無段階調整",
+      description: null,
       image:       "https://thumbnail.image.rakuten.co.jp/@0_mall/bruno-official/cabinet/item_main/2019/white/7760307.jpg?_ex=240x240",
       rakutenUrl:  "https://a.r10.to/h5EeEb",
       amazonUrl:   "https://amzn.to/41t6AnV"
